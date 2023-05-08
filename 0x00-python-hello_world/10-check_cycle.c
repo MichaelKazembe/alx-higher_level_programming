f8a3af7a09350059d8825b77ebe4b39ec929b8f9
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-08 14:32:49 -0700

    bug fixed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,24 +9,21 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *slow = NULL;
+	listint_t *fast = NULL;
+
+	if (!list)
+		return (0);
 
 	slow = list;
 	fast = list;
 
-	if (!list)
-	{
-		return (0);
-	}
-
-	while (slow && fast->next)
+	while (fast && slow && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
-		{
 			return (1);
-		}
 	}
 
 	return (0);
