a0eeeacb5f238b3b2dcd2528abeadd9b527056de
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-09 15:26:21 -0700

    code change

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for first_digit in range(0, 10):
-    for sec_digit in range(first_digit + 1, 10):
+for first_digit in range(0, 10, 1):
+    for sec_digit in range(first_digit + 1, 10, 1):
         if first_digit == 8:
             print("{}{}".format(first_digit, sec_digit))
             break
