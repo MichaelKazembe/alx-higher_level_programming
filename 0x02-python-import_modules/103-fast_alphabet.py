a5d545ba249052ab2695a47b4580bc4ac231e050
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-12 11:46:41 -0700

    requirement check

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-from print_103 import print_upper_alpha
-print_upper_alpha()
+import print_103
--- 0x02-python-import_modules/print_103.py
+++ 0x02-python-import_modules/print_103.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-
-"""
-   Prints the alphabet in uppercase
-   followed by a new line
-
-"""
-
-
-def print_upper_alpha():
-    for upper_alpha in range(65, 91):
-        print("{}".format(chr(upper_alpha)), end='')
+for upper_alpha in range(65, 91):
+    print("{}".format(chr(upper_alpha)), end='')
+print()
