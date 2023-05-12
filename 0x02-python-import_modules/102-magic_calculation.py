0143a2b00c2b6d9e3b96467fbed03e9d8d04c4f3
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-05-12 22:03:55 +0200

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
-a = 0
-c = 0
-if a < b:
-    c = add(a, b)
-    for i in range(4, 6):
-        c = add(c, i)
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def magic_calculation_102(a, b):
+    c = 0
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+    else:
+        return sub(a, b)
+    
     return c
-else:
-    return sub(a, b)
