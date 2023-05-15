8f457c82f40e10933c525680f423c8b161e34c93
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-15 08:27:40 -0700

    handle integer not in row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+        for integer in row:
+            print("{:d}".format(integer), end="")
+            if(integer is not row[len(row)-1]):
+                print(" ", end="")
         print()
