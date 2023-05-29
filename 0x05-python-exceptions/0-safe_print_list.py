00a1bc6abd53e83e5a9ec043ce72b18b44b91fe9
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-29 13:04:04 -0700

    added shebang

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed = 0
     try:
         for i in range(x):
             try:
                 element = my_list[i]
-                print(element, end= "")
+                print(element, end="")
                 printed += 1
             except IndexError:
                 break
