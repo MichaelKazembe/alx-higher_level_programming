63b17c370b74e49c654b2b5085afb420f4535568
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-29 12:59:32 -0700

    safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    try:
+        for i in range(x):
+            try:
+                element = my_list[i]
+                print(element, end= "")
+                printed += 1
+            except IndexError:
+                break
+
+    except TypeError:
+        return printed
+
+    print()
+    return printed
