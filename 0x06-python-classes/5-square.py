fde49eaa2cbbef5e2418c70ea4828759045c8751
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-30 15:59:12 -0700

    print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" creates class Square """
+
+
+class Square:
+    """ class Square """
+    def __init__(self, size=0):
+        """ class Square object """
+        self.__size = size
+
+    @property
+    def size(self):
+        """get size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ public instant method area()"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """ public instant method my_print() """
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
