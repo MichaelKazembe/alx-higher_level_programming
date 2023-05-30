5271bfaef4c2aeef0fa4c84a2df64ff439ebe79a
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-30 15:08:46 -0700

    private square attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" creates class Square """
+
+
+class Square:
+    """ class Square """
+    def __init__(self, size):
+        """ class Square object """
+        self.__size = size
