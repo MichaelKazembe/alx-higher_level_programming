ccf57ce04ed6d5d8bf20f85462009052f1c85961
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-30 17:39:33 -0700

    python code from bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import math
+""" Create class"""
+
+
+class MagicClass:
+    """Magic class from given bytecode output"""
+
+    def __init__(self, radius=0):
+        """initialize radius object"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """returns area of circle"""
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        """returns circumference of circle"""
+        return 2 * math.pi * self._MagicClass__radius
