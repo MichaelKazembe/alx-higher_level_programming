64c89040ac5380f1e177952146404f09e8b4d3ac
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-06-09 12:47:00 +0200

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -18,10 +18,10 @@ def say_my_name(first_name, last_name=""):
         TypeError: If first_name is not a string or last_name is not a string.
 
     Examples:
-        >>> say_my_name("Peter", "Pan
+        >>> say_my_name("Peter", "Pan")
         My name is John Doe
 
-        >>> say_my_name("Michael
+        >>> say_my_name("Michael")
         My name is Michael
 
         >>> say_my_name(123, "Smith")
