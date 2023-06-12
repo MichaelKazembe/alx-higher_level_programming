769ba6d8e7d62b690bcc743eb31720fa5409ae16
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-12 14:24:12 -0700

    edge cases

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry():
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Public instance method area """
+        """ Public instance method integer validator """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
