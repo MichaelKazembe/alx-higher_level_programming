6b248b7c2db9d0474f9cf5dd918ca3a24f0095f6
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-12 12:38:50 -0700

    pycodestyle error fixed

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,3 @@ indirectly) from the specified class ; otherwise False
 def inherits_from(obj, a_class):
     """ function inherits_from """
     return (isinstance(obj, a_class)) and issubclass(type(obj), a_class)
-
