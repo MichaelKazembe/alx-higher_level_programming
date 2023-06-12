903fc6dcf8d7299b8df62892acf917666f87ddfe
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-12 12:42:22 -0700

    edge case check

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 A function that returns True if the object is
-an instance of a class that inherited (directly or
+an instance of a class that inherited (directly or 
 indirectly) from the specified class ; otherwise False
 
 """
@@ -9,4 +9,6 @@ indirectly) from the specified class ; otherwise False
 
 def inherits_from(obj, a_class):
     """ function inherits_from """
-    return (isinstance(obj, a_class)) and issubclass(type(obj), a_class)
+    if type(obj) == a_class:
+        return True
+    return issubclass(type(obj), a_class)
