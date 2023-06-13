89d53e737577bbd6dbb4413e369313ec330672df
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-13 15:22:15 -0700

    bug fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,49 +1,60 @@
 #!/usr/bin/python3
+""" 
+Module to print status codes. 
 
 """
-a script that reads stdin line by line and computes metrics
-
-"""
-
 import sys
 
 
-def print_metrics(total_size, status_codes):
-    """Prints the metrics"""
-    print("File size: {}".format(total_size))
-    sorted_codes = sorted(status_codes.keys())
-    for code in sorted_codes:
-        count = status_codes[code]
-        print("{}: {}".format(code, count))
+class Magic:
+    """ Class magic"""
+    def __init__(self):
+        """ Init method """
+        self.dict = {}
+        self.size = 0
+
+    def init_dict(self):
+        """ Initialize dict """
+        self.dict['200'] = 0
+        self.dict['301'] = 0
+        self.dict['400'] = 0
+        self.dict['401'] = 0
+        self.dict['403'] = 0
+        self.dict['404'] = 0
+        self.dict['405'] = 0
+        self.dict['500'] = 0
+
+    def add_status_code(self, status):
+        """ add repeated numbers to the status code """
+        if status in self.dict:
+            self.dict[status] += 1
+
+    def print_info(self, sig=0, frame=0):
+        """ print status code """
+        print("File size: {:d}".format(self.size))
+        for key in sorted(self.dict.keys()):
+            if self.dict[key] is not 0:
+                print("{}: {:d}".format(key, self.dict[key]))
 
 
-def compute_metrics():
-    """Computes the metrics"""
-    total_size = 0
-    status_codes = {}
+if __name__ == "__main__":
+    magic = Magic()
+    magic.init_dict()
+    n_lines = 0
 
     try:
-        line_count = 0
         for line in sys.stdin:
-            line = line.strip()
-            if line:
-                parts = line.split(" ")
-                size = int(parts[-1])
-                status_code = parts[-2]
+            if n_lines % 10 == 0 and n_lines is not 0:
+                magic.print_info()
 
-                total_size += size
-
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-                else:
-                    status_codes[status_code] = 1
-
-                line_count += 1
-
-                if line_count % 10 == 0:
-                    print_metrics(total_size, status_codes)
+            try:
+                list_line = [x for x in line.split(" ") if x.strip()]
+                magic.add_status_code(list_line[-2])
+                magic.size += int(list_line[-1].strip("\n"))
+            except:
+                pass
+            n_lines += 1
     except KeyboardInterrupt:
-        print_metrics(total_size, status_codes)
-
-
-compute_metrics()
+        magic.print_info()
+        raise
+    magic.print_info()
