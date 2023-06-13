6ac558fb82da4de6a06c2fb4905c1078d82b8803
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-13 11:09:18 -0700

    to json from python  object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import json
+
+"""
+a function that returns the JSON representation
+of an object (string)
+
+"""
+
+
+def to_json_string(my_obj):
+    """function to_json_string"""
+    return json.dumps(my_obj)
