a0ae763fe375cd1a2c681f6052440c7ca710c96f
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-13 14:31:32 -0700

    test

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+a function that inserts a line of text to a file,
+after each line containing a specific string
+
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ function append_after """
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    with open(filename, "w") as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + "\n")
