69842f0cf20877f318d3c1b3370116467d78cd7d
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-13 14:36:12 -0700

    encoding

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,10 +9,10 @@ after each line containing a specific string
 
 def append_after(filename="", search_string="", new_string=""):
     """ function append_after """
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="UTF-8") as file:
         lines = file.readlines()
 
-    with open(filename, "w") as file:
+    with open(filename, "w", encoding="UTF-8") as file:
         for line in lines:
             file.write(line)
             if search_string in line:
