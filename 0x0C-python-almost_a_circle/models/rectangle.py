e2ccb1db492d7a505da9ec4129ead1375abe4e3a
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-19 10:33:21 -0700

    added brackets on ({id})

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """ String representation of the rectangle object """
-        return ("[Rectangle] {} {}/{} - {}/{}".format(
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id,
             self.x,
             self.y,
