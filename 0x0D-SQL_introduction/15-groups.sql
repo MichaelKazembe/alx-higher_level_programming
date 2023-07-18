3298066e077aca12cdeeaf45453ea4824fb2d94f
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-07-19 00:55:43 +0200

    Update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Counts the number of records with the same score in the table
-SELECT score, COUNT(*) AS record
+SELECT score, COUNT(*) AS record_num
 FROM second_table 
 GROUP BY score
-ORDER BY record DESC;
+ORDER BY record_num DESC;
