0a5b0af5c2c6fd52244e1210ffe17d74da9d504c
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-14 16:13:32 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+Prints statements about C, Python, and JavaScript
+using an array and a loop
+*/
+
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (const line of lines) {
+  console.log(line);
+}
