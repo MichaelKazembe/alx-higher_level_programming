bfc0b4ceddb4e8686676fb5d97863254eefa817e
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-14 16:29:06 -0700

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,18 +6,12 @@
   If the argument is not a valid number, it displays an error message.
 */
 
-const args = process.argv.slice(2);
+const num = parseInt(process.argv[2]);
 
-if (args.length !== 1) {
-  console.log('Missing number of occurrences');
-} else {
-  const num = parseInt(args[0]);
-
-  if (isNaN(num)) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < num; i++) {
-      console.log('C is fun');
-    }
+if (!Number.isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
 }
