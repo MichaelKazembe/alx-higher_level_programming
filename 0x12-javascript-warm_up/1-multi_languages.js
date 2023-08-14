799cd347964ddb32ddf55a54f5aada9fc8e49329
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-14 15:20:37 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+prints statements about c, python, and javascript
+*/
+
+const cLine = 'C is fun';
+const pythonLine = 'Python is cool';
+const javascriptLine = 'JavaScript is amazing';
+console.log(cLine);
+console.log(pythonLine);
+console.log(javascriptLine);
