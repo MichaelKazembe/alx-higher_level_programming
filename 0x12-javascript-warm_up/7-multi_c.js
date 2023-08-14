f152f504a33115d9d34bd530f362458b6633d7c8
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-14 16:21:11 -0700

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+/*
+  This script prints "C is fun" a specified number of times.
+  The number of times is determined by the first command-line argument.
+  If the argument is not a valid number, it displays an error message.
+*/
+
+const args = process.argv.slice(2);
+
+if (args.length !== 1) {
+  console.log('Missing number of occurrences');
+} else {
+  const num = parseInt(args[0]);
+
+  if (isNaN(num)) {
+    console.log('Missing number of occurrences');
+  } else {
+    for (let i = 0; i < num; i++) {
+      console.log('C is fun');
+    }
+  }
+}
