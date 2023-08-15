c20169568e4f97179da3cab069280b75625536b6
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-14 17:37:57 -0700

    103-object_fct.js

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+/*
+Function that increments the integer value
+*/
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
