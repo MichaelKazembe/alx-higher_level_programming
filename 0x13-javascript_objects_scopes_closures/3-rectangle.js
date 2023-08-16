2b6037b89a4e5ff7798fe688cbfb4301b3368fcb
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-16 13:57:06 -0700

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/*
+class Rectangle that defines a rectangle:
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+ print () {
+    for (let j = 0; j < this.height; j++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
