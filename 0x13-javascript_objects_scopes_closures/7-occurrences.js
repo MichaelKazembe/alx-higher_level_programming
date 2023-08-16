3f29cc3d380aeed85221dde1bc47a2d3a5756c44
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-16 14:34:30 -0700

    7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/*
+Function returns number of occurrences in a list
+*/
+
+'use strict';
+exports.nbOccurrences = function (list, searchElement) {
+  let occurrences = 0;
+
+  for (let j = 0; j < list.length; j++) {
+    if (list[j] === searchElement) {
+      occurrences++;
+    }
+  }
+  return occurrences;
+};
