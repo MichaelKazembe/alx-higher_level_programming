74722ec47315273b98684587be73ef19e571ea7f
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-20 16:44:55 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        raise Exception("Need 4 arguments!")
+
+    myDataBase = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = myDataBase.cursor()
+
+    state_name = sys.argv[4]
+
+    # Use format to create the SQL query with user input
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+    cursor.execute(query, (state_name,))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    myDataBase.close()
