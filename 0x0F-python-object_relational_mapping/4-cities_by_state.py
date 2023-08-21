eb1e4d36be85dcbec578cf9294f335c51c07ba7d
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-20 17:26:19 -0700

    4-cities_by_state.

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_4_usa.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        raise Exception("Need 3 arguments!")
+
+    myDataBase = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = myDataBase.cursor()
+
+    # Use execute() once to fetch all cities and sort by cities.id
+    cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    myDataBase.close()
