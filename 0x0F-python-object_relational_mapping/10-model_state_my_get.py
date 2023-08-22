9eaab34dac715d4d9952a40c8d43fb8674c064e3
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-22 06:27:05 -0700

    10-model_state_my_get.py update

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,7 +9,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 5:
         raise Exception("Need 4 arguments!")
 
@@ -21,19 +21,18 @@ if __name__ == "__main__":
     # Create an engine
     URL = "mysql+mysqldb://{}:{}@localhost:3306/{}"
     engine = create_engine(URL.format(user, passwd, db))
-    Base.metadata.create_all(engine)
 
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Fetch the State object with the given name
-    state = session.query(State).filter(State.name == search_name).first()
-
-    if state:
-        print("{}".format(state.id))
+    states = session.query(State).filter(State.name == search_name).order_by(State.id)
+    
+    if states is not None and states.count() > 0:
+        for state in states:
+            print('{}'.format(state.id))
     else:
-        print("Not found")
+        print('Not found')
 
     # Close the session
     session.close()
