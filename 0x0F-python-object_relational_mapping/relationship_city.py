0a721c53a750f18eee6da94bbc2291690b517eea
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-24 16:35:36 -0700

    update

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-# Defines a City model
-# Inherits from SQLAlchemy Base and links to the MySQL table cities
+""" Class definition of a State and an instance Base = declarative_base(),"""
 
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import ForeignKey, Integer, String, Column
+from relationship_state import Base, State
+from sqlalchemy.orm import relationship
 
 
 class City(Base):
-    """
-    Represents a city for a MySQL database.
-    """
+    """ Cities class """
     __tablename__ = "cities"
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+    def __repr__(self):
+        return("<City(id="%s", name="%s", state_id="%s")>"
+               % (self.id, self.name, self.state_id))
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-# Defines a State model
-# Inherits from SQLAlchemy Base and links to the MySQL table states
+"""Class definition of a State and an instance Base = declarative_base() """
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Integer, String, Column
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
 class State(Base):
-    """ Represents a state for a MySQL database """
+    """ state class """
     __tablename__ = "states"
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
 
-    cities = relationship("City", backref="state", cascade="all, delete")
+    def __repr__(self):
+        return("<States(id="%s", name="%s")>" % (self.id, self.name))
