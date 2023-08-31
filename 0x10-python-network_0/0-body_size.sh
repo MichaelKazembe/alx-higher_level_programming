784a8d4fc3085819e199400501efcc80c35f9d68
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-09-01 00:59:11 +0200

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # displays the size of the body of the response(Content-length)
 
-url="$1"
-curl -sI "$url" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
