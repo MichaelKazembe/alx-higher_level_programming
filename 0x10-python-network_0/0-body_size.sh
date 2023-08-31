142430b886dabc2cbec1045e9f80d3502fabff80
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-09-01 01:32:10 +0200

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL
 # displays the body of the response
-
 url="$1"
-curl -sI "$url" | grep -i 'Content-Length' | awk '{print $2}'
+curl -sI "$url" | grep -i 'Content-Length' | cut -f2 -d ':'
