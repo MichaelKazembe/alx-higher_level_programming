dc292a3c5e81750dfc18d215a774a9235454860c
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-05 12:22:42 -0700

    pycodestyle

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,7 +9,7 @@ import requests
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
 
-    content= requests.get(url)
+    content = requests.get(url)
 
     print("Body response:")
     print("\t- type:", type(content.text))
