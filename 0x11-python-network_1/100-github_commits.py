f6e0f830668c4f59068f8b8ed2c81031d24ffb28
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-05 13:37:55 -0700

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""lists the 10 most recent commits on a given GitHub repository.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        owner_name, repo_name)
+
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
