a2bad7ae96347fcb38e794646ad564680a17a79b
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-05 12:20:26 -0700

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script fetches the status from https://alx-intranet.hbtn.io/status
+using the requests package. It then displays information about
+the response body.
+"""
+import requests
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    content= requests.get(url)
+
+    print("Body response:")
+    print("\t- type:", type(content.text))
+    print("\t- content:", content.text)
