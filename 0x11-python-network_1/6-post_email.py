d092a834bb87b9034e8d1e9e34739d1e0531a9ad
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-05 13:01:06 -0700

    update

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
+sends a POST request to the passed URL with the email
+as a parameter, and finally displays the body of the response.
 """
 import requests
 import sys
