8395c77f6ad3d3c6f2e0bcd20d99db9db42b3380
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-10-10 01:02:53 +0200

    Update 103-script.js

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,19 +1,18 @@
+$(function() {
+  function fetchTranslation() {
+    var languageCode = $('#language_code').val();
+    var apiUrl = 'https://www.fourtonfish.com/hellosalut/hello/?lang=' + languageCode;
+    $.get(apiUrl, function(data) {
+      $('#hello').text(data.hello);
+    });
+  }
+  $('INPUT#btn_translate').click(function() {
+    fetchTranslation();
+  });
 
-n() {
-	  function fetchTranslation() {
-		      var languageCode = $('#language_code').val();
-		      var apiUrl = 'https://www.fourtonfish.com/hellosalut/hello/?lang=' + languageCode;
-		      $.get(apiUrl, function(data) {
-			            $('#hello').text(data.hello);
-			          });
-		    }
-	  $('INPUT#btn_translate').click(function() {
-		      fetchTranslation();
-		    });
-
-	  $('#language_code').keydown(function(event) {
-		      if (event.keyCode === 13) { // 13 corresponds to the ENTER key
-			            fetchTranslation();
-			          }
-		    });
+  $('#language_code').keydown(function(event) {
+    if (event.keyCode === 13) { // 13 corresponds to the ENTER key
+      fetchTranslation();
+    }
+  });
 });
